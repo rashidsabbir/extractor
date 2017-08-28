af85ba86cd08d47b60202990ead362a22981f01f
LinuxWorkstation <rashidsabbir@gmail.com>
2017-08-28 10:10:53 -0700

    Added to list of necessary packages

--- install_packages.sh
+++ install_packages.sh
@@ -7,3 +7,9 @@ pip install stemming
 pip install requests
 pip install inflect
 apt-get install python-tk
+apt install twine
+apt-get install python3-setuptools
+pip install labkey
+pip install PyDictionary
+pip install sklearn
+pip install scipy
